6fc3a6f7b65d15936e1cde6ca0413f194785408f
Marcelo Rezende <malvre@gmail.com>
2016-09-26 13:12:08 -0300

    Create models.java

---
+++ models.java
@@ -0,0 +1,18 @@
+public class Question {
+
+	String title;
+	String link;
+
+	@Override
+	public String toString() {
+		return (title);
+	}
+}
+
+
+
+public class StackOverflowQuestions {
+	
+  public List<Question> items;
+  
+}
